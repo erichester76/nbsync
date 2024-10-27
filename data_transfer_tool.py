a3994a81ba39c214241dd986ebd6a08000fdbf77
Eric Hester <eric.hester@umbrella.tech>
2024-10-27 18:11:23 -0400

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -186,7 +186,7 @@ class DataTransferTool:
 
                 # Check if the object exists
                 found_object = found_object.first() if hasattr(found_object, 'first') else None
-                pprint(found_object)
+                pprint.pp(found_object)
                 if found_object:
                     # If object exists, return its ID
                     value = found_object.id
