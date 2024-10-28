4b4b6f5f0647d7a284e404df6c2885b030de23c1
Eric Hester <eric.hester@umbrella.tech>
2024-10-28 12:44:07 -0400

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -19,6 +19,7 @@ class DataTransferTool:
         self.dry_run = dry_run  # Store the dry_run flag
         self.sources = {}
         self.mapped_data = {}
+        self.DEBUG = 0
 
     def get_nested_function(self, api_client, function_path):
         """Recursively get a function from the API client."""
@@ -120,18 +121,18 @@ class DataTransferTool:
                 if "regex_replace" in trans:
                     # Extract pattern and replacement from the transform rule
                     pattern, replacement = re.findall(r"regex_replace\('(.*?)',\s*'(.*?)'\)", trans)[0]
-                    print(f'Applying regex: {value} {pattern} {replacement}')
+                    if self.DEBUG == 1: print(f'Applying regex: {value} {pattern} {replacement}')
                     value = re.sub(pattern, replacement, value)
                 
                 elif trans == "slugify":
                     value = re.sub(r'\W+', '-', value.lower())
-                    print(f'Slugifying value: {value}')
+                    if self.DEBUG == 1: print(f'Slugifying value: {value}')
                 
                 elif trans == "expand":
                     expand_field = obj_config['mapping'].get(field_name, {}).get('expand_reference')
                     if expand_field:
                         value = {expand_field: value}
-                        print(f"Expanding field {field_name} as reference: {value}")
+                        if self.DEBUG == 1: print(f"Expanding field {field_name} as reference: {value}")
                     else:
                         raise ValueError(f"Expand transform requires 'expand_reference' key in mapping for {field_name}")
 
@@ -139,7 +140,7 @@ class DataTransferTool:
                     # Get the list of source fields to concatenate
                     fields_to_concat = obj_config['mapping'][field_name]['source']
                     delimiter = trans.split('concat(')[1].strip(" )")
-                    print(f"Concating fields {field_name} : {value}")
+                    if self.DEBUG == 1: print(f"Concating fields {field_name} : {value}")
                     # Get the values of the fields to concatenate
                     values = [self.get_nested_attribute(item, field, None) for field in fields_to_concat]
                     value = delimiter.join([str(v) for v in values if v])  # Join non-empty values
@@ -167,7 +168,7 @@ class DataTransferTool:
                         for included_field in obj_config['mapping'][field_name].get('included_fields', []):
                             field_name_for_nesting = included_field.get('field')
                             break
-                        print(f"Added {additional_data} and {field_name_for_nesting}")
+                        if self.DEBUG == 1: print(f"Added {additional_data} and {field_name_for_nesting}")
                         
                     for _, nested_value in additional_data.items():
                         nested_value = re.sub(r'\W+', '-', nested_value.lower())
@@ -176,7 +177,7 @@ class DataTransferTool:
                     try:
                         found_object = find_function(**filter_params)
                     except Exception as e:
-                        print(f"Error calling find_function: {str(e)}")
+                        if self.DEBUG == 1: print(f"Error calling find_function: {str(e)}")
                         raise
                     
                     if found_object:
@@ -187,7 +188,7 @@ class DataTransferTool:
                         else:
                             create_data = {lookup_param_name: lookup_param_value, 'slug': re.sub(r'\W+', '-', lookup_param_value.lower())}
 
-                        print(f'Creating object: {create_data}')
+                        if self.DEBUG == 1: print(f'Creating object: {create_data}')
                         created_object = create_function(create_data)
                         value = created_object.id if hasattr(created_object, 'id') else None
                     
@@ -196,7 +197,7 @@ class DataTransferTool:
     def process_mappings(self):
         """Process the mappings defined in the object_mappings section of the YAML."""
         for obj_type, obj_config in self.config['object_mappings'].items():
-            print(f"Process Object Group {obj_type}")
+            if self.DEBUG == 1: print(f"Process Object Group {obj_type}")
             source = self.sources[obj_config['source_api']]
             for source_client in source.clients:
                 source_data = source.fetch_data(obj_config, source_client)
@@ -223,7 +224,7 @@ class DataTransferTool:
 
                             # Handle object-like data sources (e.g., vim.VirtualMachine)
                             else:
-                                print(f"Mapping source field {field_info['source']} to {dest_field}")
+                                if self.DEBUG == 1: print(f"Mapping source field {field_info['source']} to {dest_field}")
                                 
                                 # Check if the source is a list (for concatenation)
                                 if isinstance(field_info['source'], list):
@@ -234,20 +235,20 @@ class DataTransferTool:
                                     source_value = self.get_nested_attribute(item, field_info['source'], None)
 
                             # Debugging: Log the value we're about to map
-                            print(f"Mapped value for {dest_field}: {source_value}")
+                            if self.DEBUG == 1: print(f"Mapped value for {dest_field}: {source_value}")
 
                             if ('transform_function' in field_info):
                                 transform = field_info.get('transform_function')
-                                print(f"Applying transform to field {dest_field} {source_value} {transform}")
+                                if self.DEBUG == 1: print(f"Applying transform to field {dest_field} {source_value} {transform}")
                                 mapped_data[dest_field] = self.apply_transform_function(source_value, transform, obj_config, dest_field, item)
-                                print(f'Value is now: {mapped_data[dest_field]}')
+                                if self.DEBUG == 1: print(f'Value is now: {mapped_data[dest_field]}')
                             else:
-                                print(f"Directly mapping {source_value} to {dest_field}")
+                                if self.DEBUG == 1: print(f"Directly mapping {source_value} to {dest_field}")
                                 mapped_data[dest_field] = source_value
                                 
-                        print(f"Looking up {mapped_data.get('name')} with filter params: {mapped_data}")
+                        if self.DEBUG == 1: print(f"Looking up {mapped_data.get('name')} with filter params: {mapped_data}")
                         object_id = self.create_or_update(destination_client, find_function, create_function, update_function, mapped_data)
-                        print(f"Processed object with ID: {object_id}")
+                        if self.DEBUG == 1: print(f"Processed object with ID: {object_id}")
 
     def create_or_update(self, api_client, find_function_path, create_function_path, update_function_path, mapped_data):
         """Create or update objects in the destination API."""
@@ -271,32 +272,32 @@ class DataTransferTool:
                 additional_data = self.get_included_fields_data(self.config['object_mappings'], dest_field, mapped_data)
                 filter_params.update(additional_data)
 
-        print(f"Looking up {key_field} with filter params: {filter_params}")
+        if self.DEBUG == 1: print(f"Looking up {key_field} with filter params: {filter_params}")
         
         # Search for the object using the find function (e.g., dcim.devices.filter)
         try:
             found_object = find_function(**filter_params)
         except Exception as e:
-            print(f"Error calling find_function: {str(e)}")
+            if self.DEBUG == 1: print(f"Error calling find_function: {str(e)}")
             raise
                 
         # Check if the result set is not empty (using .first() if available)        
         if found_object:
             existing_object = list(found_object)[0]
             if self.dry_run:
-                print(f"[DRY RUN] Would update object {existing_object.id} with data: {mapped_data}")
+                if self.DEBUG == 1: print(f"[DRY RUN] Would update object {existing_object.id} with data: {mapped_data}")
             else:
                 #mapped_data.pop('name', None)  # Remove 'name' field if it's not required
                 #mapped_data.pop('model', None)  # Remove 'model' field if not required
-                print(f"Updating object {existing_object.id} using {update_function_path} with data: {mapped_data}")
+                if self.DEBUG == 1: print(f"Updating object {existing_object.id} using {update_function_path} with data: {mapped_data}")
                 #update_function = self.get_nested_function(api_client, update_function_path)
                 #update_function(existing_object.id, **self.sanitize_data(mapped_data))
             return existing_object.id
         else:
             if self.dry_run:
-                print(f"[DRY RUN] Would create new object with {create_function_path}: {mapped_data}")
+                if self.DEBUG == 1: print(f"[DRY RUN] Would create new object with {create_function_path}: {mapped_data}")
             else:
-                print(f"Creating new object with {create_function_path}: {mapped_data}")
+                if self.DEBUG == 1: print(f"Creating new object with {create_function_path}: {mapped_data}")
                 create_function = self.get_nested_function(api_client, create_function_path)
                 new_object = create_function(self.sanitize_data(mapped_data))
                 return new_object.id
