43f629fc5e9856bb84a05929a8d250bbf28c89a5
Eric Hester <eric.hester@umbrella.tech>
2024-10-29 09:15:48 -0400

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -301,8 +301,8 @@ class DataTransferTool:
     def process_mappings(self):
         """Process the mappings defined in the object_mappings section of the YAML."""
         for obj_type, obj_config in self.config['object_mappings'].items():
-            self.sources[obj_config['source_api'].authenticate()
-
+           
+            self.sources[obj_config['source_api']].authenticate()
             source = self.sources[obj_config['source_api']]
 
             for source_client in source.clients:
