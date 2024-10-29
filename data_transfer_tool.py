0fe82cacb9bffd373cfc8097ea1acd29a59cf439
Eric Hester <eric.hester@umbrella.tech>
2024-10-29 09:15:10 -0400

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -301,10 +301,12 @@ class DataTransferTool:
     def process_mappings(self):
         """Process the mappings defined in the object_mappings section of the YAML."""
         for obj_type, obj_config in self.config['object_mappings'].items():
+            self.sources[obj_config['source_api'].authenticate()
+
             source = self.sources[obj_config['source_api']]
+
             for source_client in source.clients:
                 print(f"Processing {obj_type}..")
-                source.authenticate()
                 source_data = source.fetch_data(obj_config, source_client)
                 # Log the raw data fetched from the source
                 destination_api = self.sources[obj_config['destination_api']]
