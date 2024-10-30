b2bf3620b4d01a5f18b41774d76f7ce69e53615b
Eric Hester <eric.hester@umbrella.tech>
2024-10-30 06:19:57 -0400

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -48,7 +48,7 @@ class DataTransferTool:
     def __init__(self, yaml_file, dry_run):
         template = env.get_template(yaml_file)
         #rendered_yaml = template.render({**{k: v for k, v in os.environ.items()}, 'render_mappings': False})
-        self.config = yaml.load(template, Loader=yaml.FullLoader)
+        self.config = yaml.load(yaml_file, Loader=yaml.FullLoader)
         self.dry_run = dry_run  # Store the dry_run flag
         self.sources = {}
         self.mapped_data = {}
