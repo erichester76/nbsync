db53c3fabcd56de91e3c09ac56409abe175340a0
Eric Hester <eric.hester@umbrella.tech>
2024-10-31 19:49:45 -0400

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -289,6 +289,9 @@ class DataTransferTool:
         find_function = self.get_nested_function(api_client, find_function_path)
         # Automatically extract the first field from mapped_data as the key field
         key_field = list(mapped_data.keys())[0]
+        if not mapped_data[key_field]: 
+            return None
+        
         print (f'seatching for {key_field}, {mapped_data[key_field]}')
         filter_params = {key_field: mapped_data[key_field]}        
         try:
