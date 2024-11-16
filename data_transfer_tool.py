63c9dbe77fffcbed4890a086878432d427cb72f3
Eric Hester <eric.hester@umbrella.tech>
2024-11-16 11:45:40 -0500

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -13,6 +13,7 @@ import deepdiff
 import cProfile
 import pstats
 from utils.timer import Timer
+from utils.resolver import Resolver
 
 # Register custom Jinja2 filters
 
@@ -135,41 +136,6 @@ class DataTransferTool:
             
             self.sources[name].authenticate()
 
-    def resolve_nested_context(self, item):
-        """Resolve nested attributes in an object using dot notation."""
-        context = {}
-
-        def get_nested_value(obj, attr_path):
-            """Recursively get a nested value from an object or dict using dot notation."""
-            attrs = attr_path.split('.')
-            current_obj = obj
-            try:
-                for attr in attrs:
-                    if isinstance(current_obj, dict):
-                        current_obj = current_obj.get(attr)
-                    else:
-                        current_obj = getattr(current_obj, attr)
-                    if current_obj is None:
-                        break
-                return current_obj
-            except AttributeError:
-                return None
-
-        # Build the context with dot notation support for nested attributes
-        if isinstance(item, dict):
-            for key in item:
-                context[key] = get_nested_value(item, key)
-        else:
-            for attr in dir(item):
-                try:
-                    if attr.startswith('_'):
-                        continue
-                    context[attr] = get_nested_value(item, attr)
-                except Exception as e:
-                    continue
-
-        return context
-
     def process_mappings(self):
         """Process the mappings defined in the object_mappings section of the YAML."""
                 
@@ -193,18 +159,16 @@ class DataTransferTool:
                     mappings = obj_config['mapping']
 
                     for item in source_data:
-                        # Prepare the context once per item
-                        #timer.start_timer("Resolve Nested Context")
-                        context = self.resolve_nested_context(item)
-                        print(f'{context}')
-                        #timer.stop_timer("Resolve Nested Context")
+                        resolver = Resolver(item)
                         # Render each source template for all mappings at once, only once per item
                         rendered_mappings = {}
                         for dest_field, field_info in mappings.items():
                             if 'source' in field_info:
                                 source_template = field_info['source'].replace('<<', '{{').replace('>>', '}}')
                                 template = env.from_string(source_template)
-                                rendered_source_value = template.render(item)
+                                timer.start_timer("Render Source Template")
+                                rendered_source_value = template.render(resolver.to_dict())
+                                timer.stop_timer("Render Source Template")
                                 rendered_mappings[dest_field] = rendered_source_value
 
                         # Now apply any transformations/actions to the rendered mappings
--- utils/resolver.py
+++ utils/resolver.py
@@ -1,43 +1,34 @@
-from functools import lru_cache
-import dpath.util
-
 
 class Resolver:
-    def __init__(self):
-        self._cache = {}
+    def __init__(self, item):
+        self.item = item
 
-    def resolve_nested_context(self, item):
-        """Resolve nested attributes in an object using dot notation."""
-        context = {}
+    def resolve(self, attr_path):
+        """
+        Dynamically resolve a dot-notation path in an object or dictionary.
+        """
+        attrs = attr_path.split('.')
+        current_obj = self.item
+        try:
+            for attr in attrs:
+                if isinstance(current_obj, dict):
+                    current_obj = current_obj.get(attr)
+                else:
+                    current_obj = getattr(current_obj, attr, None)
+                if current_obj is None:
+                    break
+            return current_obj
+        except AttributeError:
+            return None
 
-        def get_nested_value(obj, attr_path):
-            """Recursively get a nested value from an object or dict using dot notation."""
-            attrs = attr_path.split('.')
-            current_obj = obj
-            try:
-                for attr in attrs:
-                    if isinstance(current_obj, dict):
-                        current_obj = current_obj.get(attr)
-                    else:
-                        current_obj = getattr(current_obj, attr)
-                    if current_obj is None:
-                        break
-                return current_obj
-            except AttributeError:
-                return None
+    def to_dict(self):
+        """
+        Expose a dictionary-like interface for template rendering.
+        """
+        return self  # Self acts as a resolver for unresolved fields
 
-        # Build the context with dot notation support for nested attributes
-        if isinstance(item, dict):
-            for key in item:
-                context[key] = get_nested_value(item, key)
-        else:
-            for attr in dir(item):
-                try:
-                    if attr.startswith('_') or callable(getattr(item, attr)):
-                        continue
-                    print(f'{attr} Processed')
-                    context[attr] = get_nested_value(item, attr)
-                except Exception as e:
-                    continue
+    def __getitem__(self, attr_path):
+        return self.resolve(attr_path)
 
-        return context
+    def __getattr__(self, attr):
+        return self.resolve(attr)
