87dc372dbec69306413bd3dfb509ada2c3214ce0
Eric Hester <eric.hester@umbrella.tech>
2024-11-16 12:37:28 -0500

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -166,7 +166,7 @@ class DataTransferTool:
                     mappings = obj_config['mapping']
 
                 for item in source_data:
-                    timer.start_timer("Item Processing Total")
+                    timer.start_timer(f"Item Processing Total {obj_type}")
                     rendered_mappings = {}
                     for dest_field, field_info in mappings.items():
                         if 'source' in field_info:
@@ -220,7 +220,7 @@ class DataTransferTool:
                         self.create_or_update(destination_client, find_function, create_function, update_function, mapped_data)    
                         timer.stop_timer("Create or Update")
                         
-                    timer.stop_timer("Item Processing Total")
+                    timer.stop_timer(f"Item Processing Total {obj_type}")
 
 
     def apply_transform_function(self, value, actions, obj_config, field_name, mapped_data):
