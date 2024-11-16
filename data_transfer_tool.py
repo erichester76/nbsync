cb6646f589b8a1416b07a3af89604e29bf5d33c8
Eric Hester <eric.hester@umbrella.tech>
2024-11-16 11:29:47 -0500

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -145,14 +145,14 @@ class DataTransferTool:
             current_obj = obj
             try:
                 for attr in attrs:
-                    timer.start_timer(f"Resolve Nested Context {attr}")
+                    timer.start_timer(f"Resolve Nested Context {attrs} {attr}")
                     if isinstance(current_obj, dict):
                         current_obj = current_obj.get(attr)
                     else:
                         current_obj = getattr(current_obj, attr)
                     if current_obj is None:
                         break
-                    timer.stop_timer(f"Resolve Nested Context {attr}")
+                    timer.stop_timer(f"Resolve Nested Context {attrs} {attr}")
                 return current_obj
             except AttributeError:
                 return None
