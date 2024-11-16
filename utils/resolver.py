dce62403a09f9cd4fb028dd5b79c89c42e2e6328
Eric Hester <eric.hester@umbrella.tech>
2024-11-16 12:00:57 -0500

    .

--- utils/resolver.py
+++ utils/resolver.py
@@ -1,23 +1,18 @@
+from jinja2.defaults import DEFAULT_FILTERS
+
+
 class Resolver:
     def __init__(self, item):
         self.item = item
 
     def resolve(self, attr_path):
-        """
-        Dynamically resolve a dot-notation path in an object or dictionary.
-        """
         attrs = attr_path.split('.')
         current_obj = self.item
         try:
             for attr in attrs:
-                # Check for dot-notation conflicts dynamically
                 if isinstance(current_obj, dict):
                     current_obj = current_obj.get(attr)
                 else:
-                    # Skip reserved names dynamically
-                    if attr in dir(current_obj):
-                        if callable(getattr(current_obj, attr, None)):
-                            raise AttributeError(f"Conflicting attribute: {attr}")
                     current_obj = getattr(current_obj, attr, None)
                 if current_obj is None:
                     break
@@ -25,14 +20,19 @@ class Resolver:
         except AttributeError:
             return ""
 
+    def sanitize_keys(self, data):
+        """
+        Sanitize dictionary keys to avoid conflicts with Jinja2 filters.
+        """
+
+        reserved_words = set(DEFAULT_FILTERS.keys())        
+        sanitized_data = {}
+        for key, value in data.items():
+            sanitized_key = f"{key}_" if key in reserved_words else key
+            sanitized_data[sanitized_key] = value
+        return sanitized_data
+
     def to_dict(self):
-        """
-        Expose the Resolver itself for lazy resolution in Jinja2.
-        """
+        if isinstance(self.item, dict):
+            return self.sanitize_keys(self.item)
         return self
-
-    def __getitem__(self, attr_path):
-        return self.resolve(attr_path)
-
-    def __getattr__(self, attr):
-        return self.resolve(attr)
