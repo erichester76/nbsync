b647dc621b328910ffc9b135d3fdc870b15a5a17
Eric Hester <eric.hester@umbrella.tech>
2024-11-16 10:38:16 -0500

    .

--- utils/resolver.py
+++ utils/resolver.py
@@ -1,27 +1,20 @@
 from functools import lru_cache
+import dpath.util
+
 
 class Resolver:
     def __init__(self):
         self._cache = {}
 
-    def resolve_nested_context(self, item):
-        """Resolve flat or top-level attributes and keys in an object or dict."""
+    def resolve_nested_context(self,item):
+        """Resolve nested attributes in a dictionary using dpath."""
         context = {}
 
         if isinstance(item, dict):
-            for key, value in item.items():
-                # Directly map keys to their values if they're not complex nested structures
-                if not isinstance(value, (dict, list, tuple)):
+            for key in item.keys():
+                try:
+                    value = dpath.util.get(item, key, separator='.')
                     context[key] = value
-        else:
-            attrs = [
-                attr for attr in dir(item)
-                if not attr.startswith('_') and not callable(getattr(item, attr, None))
-            ]
-            for attr in attrs:
-                value = getattr(item, attr, None)
-                # Avoid resolving nested structures
-                if not isinstance(value, (dict, list, tuple)):
-                    context[attr] = value
-
+                except KeyError:
+                    continue
         return context
