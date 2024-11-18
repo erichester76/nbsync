f8c999b3163d126c62f6a58a8d865a47251e695b
Eric Hester <eric.hester@umbrella.tech>
2024-11-17 20:05:30 -0500

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -401,7 +401,7 @@ class DataTransferTool:
 
         if found_object:
             existing_object = next(iter(found_object), None)
-            print(dir(existing_object))           
+            print(dir(found_object))           
             mapped_data['id'] = existing_object.id
             current_data = self.sanitize_data(existing_object.serialize())
             sanitized_mapped_data = self.sanitize_data(mapped_data)
