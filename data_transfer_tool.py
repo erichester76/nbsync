69edc866c9acfd2d5dd8d5439a8d6d4492df0024
Eric Hester <eric.hester@umbrella.tech>
2024-11-17 19:48:05 -0500

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -400,8 +400,18 @@ class DataTransferTool:
             raise
 
         if found_object:
-            existing_object = found_object.first()
-            mapped_data['id'] = existing_object.id
+            try:
+                # Access the first item in the RecordSet
+                existing_object = found_object[0]
+                print(f"Found object: {existing_object}")
+                if hasattr(existing_object, 'id'):
+                    mapped_data['id'] = existing_object.id
+                    print(f"Mapped ID: {mapped_data['id']}")
+                else:
+                    raise AttributeError(f"Object does not have an 'id' attribute: {existing_object}")
+            except IndexError:
+                print("No objects found in RecordSet.")
+                
             current_data = self.sanitize_data(existing_object.serialize())
             sanitized_mapped_data = self.sanitize_data(mapped_data)
             filtered_current_data = {key: current_data.get(key) for key in mapped_data}
