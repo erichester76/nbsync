5380022b0d9b742b2024e04b31b9949230b8a37c
Eric Hester <eric.hester@umbrella.tech>
2024-11-17 19:56:26 -0500

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -400,8 +400,7 @@ class DataTransferTool:
             raise
 
         if found_object:
-            existing_object = next(iter(found_object), None)
-            print(existing_object.serialize())
+            existing_object = found_object
             mapped_data['id'] = existing_object.id
             current_data = self.sanitize_data(existing_object.serialize())
             sanitized_mapped_data = self.sanitize_data(mapped_data)
