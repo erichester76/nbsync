c8feb43e1b1440005e2447733ea40421fbce5ef2
Eric Hester <eric.hester@umbrella.tech>
2024-11-24 14:11:18 -0500

    .

--- data_transfer_tool.py
+++ data_transfer_tool.py
@@ -401,7 +401,6 @@ class DataTransferTool:
                 key_field = f"{key_field}_id"
             filter_params[key_field] = value
         
-        print(f"Ilookup filter: {filter_params}")
         # Attempt to find the object
         try:
             found_object = find_function(**filter_params)
